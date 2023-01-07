3f2d14f3325a0f9b845005dd6f17a7b8a418ea9e
Zeeshan Mughal <zeeshansaaab@gmail.com>
2023-01-07 18:10:22 +0500

    Enabled Casts

--- resources/views/components/transaction-list.blade.php
+++ resources/views/components/transaction-list.blade.php
@@ -13,7 +13,7 @@
             <div class="nk-tb-item">
                 <div class="nk-tb-col">
                     <div class="nk-tnx-type">
-                        <div class="nk-tnx-type-icon @if($transaction->trx_type == 'credit')bg-success-dim text-success @elseif($transaction->trx_type == '-') bg-danger-dim text-danger @endif">
+                        <div class="nk-tnx-type-icon @if($transaction->type == 'credit') bg-success-dim text-success @elseif($transaction->trx_type == '-') bg-danger-dim text-danger @endif">
                             <em class="icon ni ni-arrow-up-right"></em>
                         </div>
                         <div class="nk-tnx-type-text">
