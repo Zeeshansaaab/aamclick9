631ccde4c780744ce9ba9442179e41ca6a60c8c3
Zeeshan Mughal <zeeshansaaab@gmail.com>
2023-01-07 17:05:27 +0500

    added enum

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -2,26 +2,26 @@
 
 namespace App\Enums;
 
-// enum Status : string {
-//     case Active = 'active';
-//     case InActive = 'inactive';
+enum Status : string {
+    case Active = 'active';
+    case InActive = 'inactive';
 
-//     public function label()
-//     {
-//         return match($this){
-//             self::Active => 'Active',
-//             self::InActive => 'Banned'
-//         };
-//     }
+    public function label()
+    {
+        return match($this){
+            self::Active => 'Active',
+            self::InActive => 'Banned'
+        };
+    }
 
-//     public function cssClass()
-//     {
-//         return match($this){
-//             self::Active => 'success text-success',
-//             self::InActive => 'danger text-danger'
-//         };
-//     }
+    public function cssClass()
+    {
+        return match($this){
+            self::Active => 'success text-success',
+            self::InActive => 'danger text-danger'
+        };
+    }
 
-//     //Get all cases Status::cases();
-//     // validation new Enum(Status::class);
-// }
+    //Get all cases Status::cases();
+    // validation new Enum(Status::class);
+}
