e4d79d667874ab363b000a2ef9a7952857b717ad
Zeeshan Mughal <zeeshansaaab@gmail.com>
2023-01-31 01:34:14 +0500

    Committee plans and installments added

--- resources/views/frontend/committee/show.blade.php
+++ resources/views/frontend/committee/show.blade.php
@@ -13,10 +13,10 @@
                 </div>
                 <div class="pricing-body">
                     <ul class="pricing-features">
-                        <li><span class="w-50" style="font-size: 12px;">Validity</span> - <span class="ml-auto">{{ $plan->validity }}</span></li>
-                        <li><span class="w-50" style="font-size: 12px;">Amount Return</span> - <span class="ml-auto">{{ $plan->amount_return }}</span></li>
-                        <li><span class="w-50" style="font-size: 12px;">Total Members</span> - <span class="ml-auto">{{ $plan->total_members }}</span></li>
-                        <li><span class="w-50" style="font-size: 12px;">Current Members</span> - <span class="ml-auto" id="total-members">{{ $plan->members_count }}</span></li>
+                        <li><span class="w-50" style="font-size: 12px;">Validity</span> - <span class="ml-auto" style="font-size: 12px;">{{ $plan->validity }}</span></li>
+                        <li><span class="w-50" style="font-size: 12px;">Amount Return</span> - <span class="ml-auto" style="font-size: 12px;">{{ $plan->amount_return }}</span></li>
+                        <li><span class="w-50" style="font-size: 12px;">Total Members</span> - <span class="ml-auto" style="font-size: 12px;">{{ $plan->total_members }}</span></li>
+                        <li><span class="w-50" style="font-size: 12px;">Current Members</span> - <span class="ml-auto" style="font-size: 12px;" id="total-members">{{ $plan->members_count }}</span></li>
                     </ul>
                 </div>
                 <div class="pricing-amount">
