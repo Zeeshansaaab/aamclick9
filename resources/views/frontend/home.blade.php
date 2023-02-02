f992407c864d3c0689103edcc738e0cb4e1af926
Zeeshansaaab <zeeshansaaab@gmail.com>
2023-02-02 22:35:03 +0500

    testing guest layout

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,3 +1,31 @@
 <x-guest-layout>
-    
+    <div class="nk-wg7">
+        <div class="nk-wg7-stats">
+            <div class="nk-wg7-title">
+                Total Deposit in Talha khan
+            </div>
+            <div class="number-lg amount">
+                Talha khan
+            </div>
+        </div>
+        <div class="nk-wg7-stats-group">
+            <div class="nk-wg7-stats w-50">
+                <div class="nk-wg7-title">Profit Balance</div>
+                <div class="number-lg">Talha khan</div>
+            </div>
+            <div class="nk-wg7-stats w-50">
+                <div class="nk-wg7-title">
+                    Plan
+                </div>
+                <div class="number">
+                    Talha Khan
+                </div>
+            </div>
+        </div>
+        <div class="nk-wg7-foot">
+            <span class="nk-wg7-note">Last login at
+                <span>19 Nov, 2019</span></span
+            >
+        </div>
+    </div>
 </x-guest-layout>
