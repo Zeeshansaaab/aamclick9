d9e53e886b9aab4701450914c2d191498d556c54
Zeeshansaaab <zeeshansaaab@gmail.com>
2023-02-02 22:03:52 +0500

    Added installments

--- database/migrations/2023_01_30_185017_create_committee_users_table.php
+++ database/migrations/2023_01_30_185017_create_committee_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('plan_id')->constrained()->cascadeOnDelete();
+            $table->integer('committee_number')->nullable();
             $table->string('status')->default('pending');
             $table->timestamps();
         });
---
+++ resources/views/frontend/committee/committee-number.blade.php
@@ -0,0 +1,9 @@
+<main>
+    <h2 class="ff-base fw-medium">
+        Fancy display heading
+        <small class="text-soft">Your Committee Number</small>
+    </h2>
+    <h2 class="h2">Random Number</h2>
+    <span id="ranNum"></span>
+    <button id="generate">Generate</button>
+</main>
