28323fb8e9b659b7d37fbfb00ea285bf970be432
talha <muhammadanaskhan4123@gmail.com>
2023-02-03 19:32:41 +0500

    homepagechanges

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,28 +1,136 @@
 <x-guest-layout>
 {{-- slider --}}
-    <div id="carouselExFade" class="carousel slide carousel-fade" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{ getFile('images/slides/slide1.jpeg') }}" class="d-block w-100" alt="carousel">
-            </div>
-            <div class="carousel-item">
-                <img src="{{ getFile('images/slides/slide2.jpeg') }}" class="d-block w-100" alt="carousel">
-            </div>
-            <div class="carousel-item">
-                <img src="{{ getFile('images/slides/slide3.jpeg') }}" class="d-block w-100" alt="carousel">
+<div id="carouselExFade" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ getFile('images/slides/slide1.jpeg') }}" class="d-block w-100" alt="carousel">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ getFile('images/slides/slide2.jpeg') }}" class="d-block w-100" alt="carousel">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ getFile('images/slides/slide3.jpeg') }}" class="d-block w-100" alt="carousel">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+{{-- sliderend --}}
+<div class="container">
+    <div class="text-center">
+        <h3 class="nk-block-title fw-normal mt-3 text-bold">WHY CHOOSE AAMCLICK?</h3>
+        <p>AAMClICK make it easy to invest money and get profit in one click</p>
+    </div>
+    <div class="row mt-5">
+        <div class="col-6 col-sm-4">
+            <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+                <div class="card-inner">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExFade" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExFade" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+        <div class="col-6 col-sm-4">
+            <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+                <div class="card-inner">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Force next columns to break to new line at md breakpoint and up -->
+        <div class="w-100 d-none d-md-block"></div>
+        <div class="col-6 col-sm-4">
+            <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+                <div class="card-inner">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-sm-4">
+            <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+                <div class="card-inner">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="w-100 d-none d-md-block"></div>
+        <div class="col-6 col-sm-4">
+            <div class="card card-bordered" style="border: 2px solid #0b3175">
+                <div class="card-inner">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-sm-4">
+            <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+                <div class="card-inner">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+            </div>
+        </div>
     </div>
-    {{-- sliderend --}}
-    <h3>
-        How AAM CLICK WORKS
-    </h3>
+</div>
+{{-- how its work --}}
+<div style="background: #0b3175">
+    <div class="container mt-5">
+        <h3 class="text-white text-center pt-5">HOW IT WORKS?</h3>
+<div class="row" style="padding: 50px">
+<div class="col-md-4">
+<div class="card bg-white" style="color: #0b3175">
+    <div class="card-header">Header
+    </div>
+    <div class="card-inner">
+        <h5 class="card-title">Primary card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>
+</div>
+
+<div class="col-md-4">
+    <div class="card bg-white" style="color: #0b3175">
+        <div class="card-header">Header
+        </div>
+        <div class="card-inner">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="card bg-white" style="color: #0b3175">
+            <div class="card-header">Header
+            </div>
+            <div class="card-inner">
+                <h5 class="card-title">Primary card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+</div>
+    </div>
+<div>
 </x-guest-layout>
