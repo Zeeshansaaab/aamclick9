fc7a4c49ed217cc308d08aee9687f5926d77ab63
talha khan <muhammadanaskhan4123@gmail.com>
2023-02-09 19:07:06 +0500

    homepagechanges

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -24,12 +24,40 @@
     {{-- sliderend --}}
     <div class="container">
         <div class="text-center">
-            <h3 class="nk-block-title fw-normal mt-3 text-bold">WHY CHOOSE AAMCLICK?</h3>
+            <h3 class="nk-block-title fw-normal mt-3 fw-normal">WHY CHOOSE AAMCLICK?</h3>
             <p>AAMClICK make it easy to invest money and get profit in one click</p>
         </div>
         <div class="row mt-5">
-            <div class="col-6 col-sm-4">
-                <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+            <div class="col-md-4">
+                <div class="card card-bordered mb-2 border border-color">
+                    <div class="card-inner">
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" height="100px" class="site-text-color">
+                            <rect x="13" y="16" width="68" height="66" rx="6" ry="6" fill="#fff" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></rect>
+                            <rect x="24" y="82" width="11" height="5" fill="#fff" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></rect>
+                            <rect x="60" y="82" width="11" height="5" fill="#fff" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></rect>
+                            <path d="M47,70.15S61.89,62.49,61.89,51V37.6L47,31.85,32.11,37.6V51C32.11,62.49,47,70.15,47,70.15Z" fill="#eff1ff" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                            <path d="M41.56,48H52.44A1.6,1.6,0,0,1,54,49.59v5.73A1.6,1.6,0,0,1,52.44,57H41.56A1.6,1.6,0,0,1,40,55.32V49.59A1.6,1.6,0,0,1,41.56,48Z" fill="#6576ff" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                            <path d="M43,48V45a4,4,0,0,1,8,0v3" fill="none" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                            <circle cx="46.67" cy="52.79" r="0.91" fill="#fff" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
+                            <circle cx="23" cy="17" r="14" fill="#fff" stroke="#6576ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
+                            <circle cx="23" cy="17" r="10.5" fill="#e3e7fe"></circle>
+                            <path d="M28.46,20.47l-4.41-4.41a3.4,3.4,0,0,0,.26-1.31A3.34,3.34,0,1,0,21,18.1a3.41,3.41,0,0,0,1.31-.27l1.44,1.45a.33.33,0,0,0,.23.09l.79,0,0,.79a.32.32,0,0,0,.09.23.27.27,0,0,0,.23.08l.79,0,0,.79a.31.31,0,0,0,.09.22.29.29,0,0,0,.22.09l.79,0,0,.79a.3.3,0,0,0,.09.24.32.32,0,0,0,.21.08h0l1.21-.14a.3.3,0,0,0,.27-.33l-.13-1.48Z" fill="#6576ff"></path>
+                            <path d="M20.56,14.09a1,1,0,0,1-1.34,0,1,1,0,0,1,0-1.35,1,1,0,1,1,1.34,1.35Z" fill="#fff"></path>
+                            <path d="M23.72,16.39h0l3.79,3.79a.22.22,0,0,1,0,.32h0a.24.24,0,0,1-.32,0l-3.75-3.76Z" fill="#fff"></path>
+                            <rect x="17.32" y="11.6" width="11" height="11" fill="none"></rect>
+                        </svg>
+                        
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card card-bordered mb-2 border-color">
                     <div class="card-inner">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2">Card subtitle</h6>
@@ -38,8 +66,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-sm-4">
-                <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+            <div class="col-md-4">
+                <div class="card card-bordered mb-2 border-color">
                     <div class="card-inner">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2">Card subtitle</h6>
@@ -48,10 +76,8 @@
                     </div>
                 </div>
             </div>
-            <!-- Force next columns to break to new line at md breakpoint and up -->
-            <div class="w-100 d-none d-md-block"></div>
-            <div class="col-6 col-sm-4">
-                <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+            <div class="col-md-4">
+                <div class="card card-bordered border-color">
                     <div class="card-inner">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2">Card subtitle</h6>
@@ -60,8 +86,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-sm-4">
-                <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+            <div class="col-md-4">
+                <div class="card card-bordered mb-2 border-color">
                     <div class="card-inner">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2">Card subtitle</h6>
@@ -70,19 +96,8 @@
                     </div>
                 </div>
             </div>
-            <div class="w-100 d-none d-md-block"></div>
-            <div class="col-6 col-sm-4">
-                <div class="card card-bordered" style="border: 2px solid #0b3175">
-                    <div class="card-inner">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-sm-4">
-                <div class="card card-bordered mb-2" style="border: 2px solid #0b3175">
+            <div class="col-md-4">
+                <div class="card card-bordered mb-2 border-color">
                     <div class="card-inner">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2">Card subtitle</h6>
@@ -94,12 +109,12 @@
         </div>
     </div>
     {{-- how its work --}}
-    <div style="background: #0b3175">
+    <div class="bg-site">
         <div class="container mt-5">
-            <h3 class="text-white text-center pt-5">HOW IT WORKS?</h3>
+            <h2 class="text-center pt-5 fw-normal text-light">HOW IT WORKS?</h2>
             <div class="row" style="padding: 100px">
                 <div class="col-md-4">
-                    <div class="card bg-white" style="color: #0b3175">
+                    <div class="card bg-white site-text-color">
                         <div class="card-header">Header
                         </div>
                         <div class="card-inner">
@@ -110,7 +125,7 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card bg-white" style="color: #0b3175">
+                    <div class="card bg-white site-text-color">
                         <div class="card-header">Header
                         </div>
                         <div class="card-inner">
@@ -121,7 +136,7 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card bg-white" style="color: #0b3175">
+                    <div class="card bg-white site-text-color">
                         <div class="card-header">Header
                         </div>
                         <div class="card-inner">
@@ -136,21 +151,76 @@
         </div>
     </div>
     </div>
-    <div class="row container-fluid" style="margin: 100px 30px 30px 30px;">
-        <div class="col-md-6">
-            <h2 class="font-weight-bold text-center">
-                ABOUT US
-            </h2>
-            <p style="line-height: 30px; margin-bottom: 8px; padding-right: 15px; padding-left: 15px;">
-                AAMCLICK is an earning website where you can earn money through investment with one click. It is a helpful website to make a good amount of profit without any trouble. Moreover, we also give you a mobile application where you can check your all investment and profit statistics. Besides, we provide a completely secure system to our users so, they do not face any kind of problem or error. Plus, in AAMCLICK, we give three plans that are Basic, Standard, and Premium, in which users get various facilities and the details of the profit on a specific investment. .
-            </p>
+    <div class=" container-fluid">
+        <div class="row margin-max-top margin-max-rbl">
+            <div class="col-md-6">
+                <h2 class="font-weight-bold text-center">
+                    ABOUT US
+                </h2>
+                <p class="mb-5 padding-max line-space">
+                    AAMCLICK is an earning website where you can earn money through investment with one click. It is a helpful website to make a good amount of profit without any trouble. Moreover, we also give you a mobile application where you can check your all investment and profit statistics. Besides, we provide a completely secure system to our users so, they do not face any kind of problem or error. Plus, in AAMCLICK, we give three plans that are Basic, Standard, and Premium, in which users get various facilities and the details of the profit on a specific investment. .
+                </p>
 
-            <a href="#" class="btn btn-xl btn-primary" style="margin-top: 20px; border:#0b3175; background-color: #0b3175;">GET STARTED</a>
+                <a href="#" class="btn btn-xl btn-primary border-color bg-site">GET STARTED</a>
+            </div>
+            <div class="col-md-6 margin-max">
+                <img src="{{ getFile('images/slides/about-us.jpeg') }}">
+
+            </div>
         </div>
-        <div class="col-md-6" style="border: 10px solid #0b3175;">
-        <!-- <img src="{{ getFile('images/slides/slide1.jpeg') }}" -->
-
+        <!-- about us End -->
+        <!-- table Starts -->
+        <h2 class="font-weight-bold text-center mt-5 mb-5">
+            PRICES </h2>
+        <div class="container">
+            <table class="table margin-max-top border-color">
+                <thead class="bg-site text-light">
+                    <tr>
+                        <th class="py-3 ">#</th>
+                        <th class="py-3">First</th>
+                        <th class="py-3">Last</th>
+                        <th class="py-3">Handle</th>
+                        <th class="py-3">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-color">
+                        <th class="py-2">1</th>
+                        <td class="py-2">Mark</td>
+                        <td class="py-2">Otto</td>
+                        <td class="py-2">@mdo</td>
+                    </tr>
+                    <tr class="border-color">
+                        <th class="py-2">2</th>
+                        <td class="py-2">Jacob</td>
+                        <td class="py-2">Thornton</td>
+                        <td class="py-2">@fat</td>
+                    </tr>
+                    <tr class="border-color">
+                        <th class="py-2">3</th>
+                        <td class="py-2">Larry</td>
+                        <td class="py-2">the Bird</td>
+                        <td class="py-2">@twitter</td>
+                    </tr>
+                    <tr class="border-color">
+                        <th class="py-2">4</th>
+                        <td class="py-2">Larry</td>
+                        <td class="py-2">the Bird</td>
+                        <td class="py-2">@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
 
         </div>
     </div>
+
+    <!-- table end -->
+
+    <!-- celander start -->
+
+
+
+    <!-- celander end -->
+
+
 </x-guest-layout>
